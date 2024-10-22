033a3f968ca07f23751b5905e64dc46db911d883
luxxy0 <lulydiaz2801@gmail.com>
2024-10-22 14:22:14 +0000

    c2-2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
