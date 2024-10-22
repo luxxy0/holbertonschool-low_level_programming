4f5a80078fffb2cb3c0d66b517de2cb6a2c78502
luxxy0 <lulydiaz2801@gmail.com>
2024-10-22 15:34:06 +0000

    c2-5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+	return (0);
+}
