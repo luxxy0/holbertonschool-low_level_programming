3e1a88f51f9d0d15b86419daf646b25dc60b523c
luxxy0 <lulydiaz2801@gmail.com>
2024-10-23 13:22:32 +0000

    c2-6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numberz
+ * Return: 0
+ */
+int main(void)
+{
+	int x2;
+
+	for (x2 = 0; x2 <= 9; x2++)
+	{
+		putchar(x2 + '0');
+	}
+	putchar('\n');
+	return (0);
+}
