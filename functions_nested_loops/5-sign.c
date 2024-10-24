65e6d0a2dcb899e7ae5190fa74d3d5adfdc9b76f
luxxy0 <lulydiaz2801@gmail.com>
2024-10-24 22:37:29 +0000

    c3-5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - NO PUEDE SER QUE SEPA HACER LO QUE ME PIDEN PERO NO SE NI QUE VERGA HAGO AAAAAAAAAAA
+ * @n: sujeto de preba 2.0
+ * Return: 0
+ */
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
