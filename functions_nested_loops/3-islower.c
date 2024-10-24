996a4436824f33cf15e115ad7485eb1a7496fe9a
luxxy0 <lulydiaz2801@gmail.com>
2024-10-24 20:43:59 +0000

    c3-3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isolwer - voy a romper la computadora
+ * @c: letra a comprobar
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' , c <= 'z')
+		return (1);
+	else
+		return (0);
+}
