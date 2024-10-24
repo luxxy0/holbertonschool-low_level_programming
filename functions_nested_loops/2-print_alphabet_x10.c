ceaf5b1dce975acfe3ddec0cfd9b33b9c60f48ee
luxxy0 <lulydiaz2801@gmail.com>
2024-10-24 20:03:53 +0000

    c3-2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,16 @@
 /**
  * print_alphabet_x10 - imprime el abc 10 veces
  */
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
 {
 	char x, az;
 
 	for (x = 0; x < 10; x++)
+	{
 		for (az = 'a'; az <= 'z'; az++)
 		{
 			_putchar(az);
 		}
-	_putchar('\n');
+		_putchar('\n');
+		}
 }
