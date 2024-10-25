62ea6d6055d4724b45cc32c637bcc2a9f2a88592
luxxy0 <lulydiaz2801@gmial.com>
2024-10-25 15:30:02 +0000

    c3-6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calcula el valor absoluto de un número enter
+ * Return: 0
+ * @n: prototipo 33-12
+ */
+int _abs(int n)
+{
+	f (n <= 0)
+		return (n * -1);
+	else
+		return (n);
+}
