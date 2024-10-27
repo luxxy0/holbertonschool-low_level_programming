20b58c6045daac5b9b18631a798c970ab533a6fd
luxxy0 <lulydiaz2801@gmail.com>
2024-10-27 23:46:56 +0000

    c3-9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,35 +4,32 @@
  */
 void times_table(void)
 {
-	int f, c, r;
+	int fila, columna, resultado;
 
-	for (f = 0; f <= 9; f++)
-    {
-        for (c = 0; c <= 9; c++)
-        {
-            r = f * c;
+	for (fila = 0; fila <= 9; fila++)
+	{
+	for (columna = 0; columna <= 9; columna++)
+	{
+	resultado = fila * columna;
 
-            if (r < 10)
-            {
-                if (c > 0)
-                {
-                    _putchar(' ');
-		    _putchar(' ');
-                }
-                _putchar(r + '0');
-            }
-            else
-            {
-                _putchar((r / 10) + '0');
-                _putchar((r % 10) + '0');
-            }
-
-            if (c < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	if (resultado < 10)
+	{
+	if (columna > 0)
+	{
+	_putchar(',');
+	 _putchar(' ');
+	 _putchar(' ');
+	}
+	_putchar(resultado + '0');
+	}
+	else
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((resultado / 10) + '0');
+	 _putchar((resultado % 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
 }
