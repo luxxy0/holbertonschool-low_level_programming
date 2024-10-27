90e138124fa0cc28371cb4732d28ba327de0be96
luxxy0 <lulydiaz2801@gmail.com>
2024-10-27 23:56:36 +0000

    c3-10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - despues de partirme la cabeza con
+ * la tabla del 9 me ponen esta boludez xd
+ * @a: tu mama
+ * @b: tu papa
+ * Return 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
