b473f27a8d5b6a4a3229dae26e64ebb9b7e4ae66
luxxy0 <lulydiaz2801@gmail.com>
2024-10-28 00:53:51 +0000

    c3-11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,35 +7,26 @@
  */
 void print_to_98(int n)
 {
-    int x;
+	int x;
 
-    if (n < 98)
-    {
-        for (x = n; x < 98; x++)
-        {
-            printf("%d", x);
-            if (x < 97)
-            {
-                printf(", ");
-            }
-        }
-        printf("%d\n", 98);
-    }
-    else if (n > 98)
-    {
-        for (x = n; x > 98; x--)
-        {
-            printf("%d", x);
-            if (x > 99)
-            {
-                printf(", ");
-            }
-        }
-        printf("%d\n", 98);
-    }
-    else
-    {
-        printf("%d\n", n);
-    }
+	if (n < 98)
+	{
+	for (x = n; x < 98; x++)
+	{
+	printf("%d, ", x);
+	}
+	printf("%d\n", 98);
+	}
+	else if (n > 98)
+	{
+	for (x = n; x > 98; x--)
+		{
+	printf("%d, ", x);
+		}
+		printf("%d\n", 98);
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
 }
-
