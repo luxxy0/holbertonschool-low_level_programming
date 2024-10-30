34553c0883f3830147bb5c4f46d19aae5b25efe4
luxxy0 <lulydiaz2801@gmail.com>
2024-10-30 16:20:02 +0000

    c-5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - repite del 0 al 14, 10 veces
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+
+				_putchar((y / 10) + 0);
+			}
+			_putchar((y % 10) + 0);
+
+		}
+		_putchar('\n');
+	}
+}
