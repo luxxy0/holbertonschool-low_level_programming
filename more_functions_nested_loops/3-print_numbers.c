5edc4e15e6b4cc613158419bdcdb8526c150ce4c
luxxy0 <lulydiaz2801@gmail.com>
2024-10-30 16:01:57 +0000

    c-3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - imprime num del 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+
+		_putchar (n + '0');
+
+	_putchar ('\n');
+}
