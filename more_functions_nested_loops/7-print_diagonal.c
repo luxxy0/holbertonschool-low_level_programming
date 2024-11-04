950f1a8fa47c64fb07b714dcae9895f7dbce718d
luxxy0 <lulydiaz2801@gmail.com>
2024-11-04 13:34:36 +0000

    c-7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - misma monda que el 6
+ * @n: dibuja
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int L, D;
+
+	if (n > 0)
+	{
+		for (L = 0; L < n; L++)
+		{
+			for (D = 0; D < L; D++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
