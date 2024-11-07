f50ae3817f2209a43177741a661a6035e07fdf70
luxxy0 <lulydiaz2801@gmail.com>
2024-11-07 14:00:21 +0000

    c-2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - devuelve longitud de una cadena
+ * @s: xd
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int lng = 0;
+
+	while (s[lng] != '\0')
+	{
+		lng++;
+	}
+}
