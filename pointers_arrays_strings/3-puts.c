4bb23bc1a7faf38e1ba50c454651d3c16f8d51f6
luxxy0 <lulydiaz2801@gmail.com>
2024-11-07 14:43:55 +0000

    c-3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - imprime una cadena
+ * @str: var
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str > 0 && *str != '\0')
+		_putchar(*str++);
+	_putchar ('\n');
+}
