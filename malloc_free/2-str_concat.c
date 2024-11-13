4b380760e852d14f014c9bfd339f52bcb835f11d
luxxy0 <lulydiaz2801@gmail.com>
2024-11-13 17:55:47 +0000

    c-1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,16 +7,16 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, x, k;
+	int i, x, h;
 	char *p;
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+	s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
-		s2 = "";
+	s2 = "";
 	}
 
 	for (i = 0; s1[i] != '\0'; i++);
@@ -26,10 +26,20 @@ char *str_concat(char *s1, char *s2)
 
 	if (p == NULL)
 	{
-		return (NULL);
+	return (NULL);
 	}
 
-	for (k = 0; k < i; k++)
+	for (h = 0; h < i; h++)
+	{
+	p[h] = s1[h];
+	}
+
+	for (h = 0; h < x; h++)
+	{
+	p[i + h] = s2[h];
+	}
+
+	p[i + x] = '\0';
 
 	return (p);
 }
