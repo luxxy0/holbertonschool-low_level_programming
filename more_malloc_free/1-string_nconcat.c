c6668668a4031d4ed5c3e8e23497a945c84000fc
luxxy0 <lulydiaz2801@gamil.com>
2024-11-14 17:32:28 +0000

    c-1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * string_nconcat - Concatena dos cadenas en una nueva memoria
+ * @s1: Primera cadena
+ * @s2: Segunda cadena
+ * @n: Número de bytes de s2 a concatena
+ * Return: Un puntero a la nueva cadena concatenada
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, x, total_len, len_s1, len_s2;
+	char *p;
+
+	if (s1 != NULL)
+	len_s1 = strlen(s1);
+	if (s2 != NULL)
+	len_s2 = strlen(s2);
+
+	if (n >= len_s2)
+		n = len_s2;
+
+	total_len = len_s1 + n + 1;
+
+	p = (char *)malloc(total_len * sizeof(char));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; i < len_s1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (x = 0; x < n; x++)
+	{
+		p[i + x] = s2[x];
+	}
+
+	p[i + x] = '\0';
+
+	return (p);
+}
+
