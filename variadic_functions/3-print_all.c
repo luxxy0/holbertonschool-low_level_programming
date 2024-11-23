e6486359afdf1d13b8ac69457c0aa71b415f9849
luxxy0 <lulydiaz2801@gmail.com>
2024-11-23 22:20:59 +0000

    c-3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,9 +9,6 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0;
 	char *str;
-	char c;
-	int num;
-	float f;
 
 	va_start(args, format);
 
@@ -20,30 +17,21 @@ void print_all(const char * const format, ...)
 		if (i > 0)
 			printf(", ");
 
-		switch (format[i])
+		if (format[i] == 'c')
+			printf("%c", va_arg(args, int));
+		else if (format[i] == 'i')
+			printf("%d", va_arg(args, int));
+		else if (format[i] == 'f')
+			printf("%f", va_arg(args, double));
+		else if (format[i] == 's')
 		{
-			case 'c':
-				c = va_arg(args, int);  
-				printf("%c", c);
-				break;
-			case 'i':
-				num = va_arg(args, int);
-				printf("%d", num);
-				break;
-			case 'f':
-				f = va_arg(args, double);
-				printf("%f", f);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
-				break;
-			default:
-				break;
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
 		}
+
 		i++;
 	}
 
