49f2826b956a5fe6eac941c22dd8ec8294547039
luxxy0 <lulydiaz2801@gmail.com>
2025-01-09 20:10:46 +0000

    tareas

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - free memory
+ * @d: pointer to dog
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
