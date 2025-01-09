08610caa8ea630676c0ed01839c3ac5be2016083
luxxy0 <lulydiaz2801@gmail.com>
2025-01-09 17:17:58 +0000

    atrasados

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s != '\0')
+	{
+		count = _strlen_recursion(s + 1);
+		count = count + 1;
+	}
+	else
+		return (0);
+
+	return (count);
+}
